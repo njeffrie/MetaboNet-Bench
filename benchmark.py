698b340fc3e853e787af91ee35b0126bdcae77c2
Nat Jeffries <natmjeffries@gmail.com>
2025-08-11 15:25:26 -0700

    Update benchmark script to use correctly preprocessed version of brown dataset.

--- benchmark.py
+++ benchmark.py
@@ -2,10 +2,12 @@ from models.gluformer import Gluformer
 import numpy as np
 from tqdm import tqdm
 from datasets import Dataset
+import pandas as pd
+import click
 
-def load_dataset(name: str):
-    parquet_files = f'data/{name}/*.parquet'
-    return Dataset.from_parquet(parquet_files)
+def load_dataset(name: str, split: str):
+    parquet_files = f'data/{name}/dataset_{split}.parquet'
+    return Dataset.from_parquet(parquet_files)['sequences']
 
 def calculate_rmse(pred, label):
     return np.sqrt(np.mean((pred - label) ** 2))
@@ -13,21 +15,21 @@ def calculate_rmse(pred, label):
 def calculate_ape(pred, label):
     return np.mean(np.abs(pred - label) / np.abs(label))
 
-def main():
-    cgm_data_set = load_dataset('Brown2019')
-    ds_len = 1000#len(cgm_data_set)
+@click.command()
+@click.option('--dataset', type=str, default='Brown2019', help='Dataset to run the benchmark on')
+def main(dataset, split='test'):
+    cgm_data_set = load_dataset(dataset, split)
+    ds_len = len(cgm_data_set)
     gluformer = Gluformer()
     horizons = [3, 6, 9, 12] # 15, 30, 45, 60 minutes.
     rmses = np.zeros((ds_len, len(horizons)))
     apes = np.zeros((ds_len, len(horizons)))
-    i = 0
-    for idx in tqdm(range(0, ds_len * 192, 192)):
-        df = cgm_data_set[idx:idx+192]
-        timestamps = df['DataDtTm'][:180]
-        glucose_values = df['CGM']
+    for i, sample in tqdm(enumerate(cgm_data_set)):
+        timestamps = pd.to_datetime(sample[1][:180]).to_list()
+        glucose_values = sample[2]
         model_input = glucose_values[:180]
         label = glucose_values[180:192]
-        subject_id = df['PtID'][0]
+        subject_id = sample[0][0]
 
         pred, log_var = gluformer.predict(subject_id, timestamps, model_input)
         pred = pred.flatten()
@@ -41,7 +43,6 @@ def main():
     np.save('apes.npy', apes)
     print(f'average mse {np.mean(rmses, axis=0)}')
     print(f'average ape {np.mean(apes, axis=0)}')
-
 
 if __name__ == "__main__":
     main()
