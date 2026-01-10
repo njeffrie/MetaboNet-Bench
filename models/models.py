d2a93b949d10b162d4e710b5d87001047d486126
Nat Jeffries <natmjeffries@gmail.com>
2026-01-10 09:42:33 -0800

    Add UniTS model.

--- models/models.py
+++ models/models.py
@@ -2,12 +2,14 @@ from models.gluformer import Gluformer
 from models.zoh import ZeroOrderHold
 from models.linear import LinearRegression
 from models.lstm import LSTM
+from models.UniTS import UniTS
 
 model_name_map = {'gluformer': Gluformer('njeffrie/Gluformer'),
                   'gluformer-tiny': Gluformer('njeffrie/Gluformer-tiny'),
                   'zoh': ZeroOrderHold(),
                   'linear': LinearRegression(15),
-                  'lstm': LSTM('njeffrie/LSTMGlucosePrediction')}
+                  'lstm': LSTM('njeffrie/LSTMGlucosePrediction'),
+                  'units': UniTS('checkpoints/units_x128_pretrain_checkpoint.pth')}
 
 def get_model(model_name: str):
     if model_name in model_name_map:
