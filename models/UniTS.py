Correct units model load.

models/UniTS.py
@@ -976,8 +976,8 @@ class UniTS:
             prompt_num = 10
             dropout = 0.1
         self.model = Model(configs_list=[configs_list], args = Args())
-        ckpt = torch.load(model_path, weights_only=False)
-        missing, unexpected = self.model.load_state_dict(ckpt, strict=Fals, map_location=device)
+        ckpt = torch.load(model_path, weights_only=False, map_location=device)
+        missing, unexpected = self.model.load_state_dict(ckpt, strict=False)
         self.model.eval()
         self.device = device
         self.model.to(device)